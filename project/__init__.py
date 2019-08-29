b390891ec28f963214efe80368c36f6be84515e9
Obeng Johnson Boateng <obengboatengjohnson@gmail.com>
2019-08-29 20:28:42 +0000

    adding update

--- project/__init__.py
+++ project/__init__.py
@@ -10,7 +10,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 # creating database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hostelfinder.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['postgres://tixzqghxamvlqb:098bf3ec80eb9e7e3fedfd883e4ba0093592fc51e662d05f173b139d132c3ce2@ec2-107-20-230-70.compute-1.amazonaws.com:5432/dbqa66b5uv87ic']
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['postgres://tixzqghxamvlqb:098bf3ec80eb9e7e3fedfd883e4ba0093592fc51e662d05f173b139d132c3ce2@ec2-107-20-230-70.compute-1.amazonaws.com:5432/dbqa66b5uv87ic']
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
